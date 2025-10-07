b5aaea565b961eed27d06921482073e36ab403b8
Song Inseok <thddlstjrchlrh@gmail.com>
2025-10-08 04:52:15 +0900

    랜덤 데이터 생성

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
+    
+    static int[] createArray(int size, int minValue, int maxValue) {
+        Random random = new Random();
+        int[] a = new int[size];
+        for (int i = 0; i < a.length; ++i) {
+            a[i] = random.nextInt(maxValue - minValue + 1) + 1;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-        // 1. 정렬할 테스트 배열을 선언하고 초기화합니다.
-        int[] a = { 4, 8, 2, 6, 10, 3, 1, 7, 9, 5 };
-
-        // 2. Sort 클래스의 sort 메서드를 호출하여 배열 a를 정렬합니다.
+        int[] a = createArray(10, 1, 10);
         Sort.sort(a);
-
-        // 3. 정렬된 결과를 보기 좋게 문자열로 변환하여 출력합니다.
         System.out.println(Arrays.toString(a));
     }
 }
